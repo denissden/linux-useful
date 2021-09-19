270a422e17003cb708f84c1c4e24b6f2cf2992d0
deniss <denissden1337@gmail.com>
2021-09-20 00:29:48 +0300

    rotate-screen-180 is now working when launched with a shortcut

--- rotate-screen-180/rotate-screen.sh
+++ rotate-screen-180/rotate-screen.sh
@@ -1,5 +1,3 @@
-echo $SCREEN_IS_INVERTED
-
 # this script rotates the screen 180 degrees while also inverting touchpad/mouse movements
 # useful for showing your screen to someone sitting in front of you by opening the lid 180 degrees
 
@@ -7,17 +5,26 @@ echo $SCREEN_IS_INVERTED
 touchpad_id=14
 prop="Coordinate Transformation Matrix"
 
-if [ $SCREEN_IS_INVERTED == 1 ]
+
+
+#if cache directory doesn't exist, create it
+cache="~/.cache/rotate-screen-180" 
+if [ ! -f $cache ]
+then
+	mkdir -p $cache
+fi
+
+
+if [ -f "${cache}/lock" ]
 then
 	export SCREEN_IS_INVERTED=0
 	xrandr -o normal
 	xinput set-prop $touchpad_id "$prop" 1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0
-	echo "normal"
+	rm "${cache}/lock"
 else	
 	export SCREEN_IS_INVERTED=1
 	xrandr -o inverted
 	xinput set-prop $touchpad_id "$prop" -1.0 0.0 1.0 0.0 -1.0 1.0 0.0 0.0 1.0
-	echo "inverted"
+	touch "${cache}/lock"
 fi
 
-echo $SCREEN_IS_INVERTED
